ead22cb071056e5968a669a5516ea8839d2c42dd
oran <oran@localhost.localdomain>
2023-12-27 19:18:21 +0200

    crawley v0.1

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# mr_crawley
+# Mr. Crawley
+---
+Simple threaded web crawler in python3
+###### Usage
+`crawley.py -h -t THREADS -a USER_AGENT -d  target_url`
+###### Options
+`target_url`             Target url to crawl
+  `-h, --help`           Show this help message and exit
+  `-t THREADS `         Number of threads to run (default=4)
+  `-a USER_AGENT`     Set a custom User-Agent string
+  `-d`                            Show debug messages (default=false)
+###### Example
+`python3 crawley.py http://example.com -t 6 -a "Mozilla/5.0" | tee crawley.out`
--- crawley.py
+++ crawley.py
@@ -1,63 +1,129 @@
 #!/usr/bin/env python3
+import os
 import re
 import sys
 import requests
+import argparse
+import threading
+from queue import Queue
 from bs4 import BeautifulSoup
+from requests.compat import urljoin
+from urllib.parse import urlparse
+from termcolor import colored
 
-def is_valid_url(target_url):
-	scheme = re.compile("^https?://[a-z 0-9]")
-	is_valid = scheme.match(target_url)
-	if not is_valid:
-		return False
-	return True
+class Crawley:
 
-def do_get(target_url):
-	try:
-		if is_valid_url(target_url):
-			# maybe add headers for stealth
-			response = requests.get(target_url)
-			html_contents = response.text
-			return html_contents
-		else:
-			print(f"[!] Not valid url scheme: {target_url}")
-	except requests.exceptions.ConnectionError:
-		print(f"[+] Failed to establish a new connection to [{target_url}]: Name or service not known")
-	
-	return False
+	def __init__(self, opts):
+		self.threads         = opts["threads"]
+		self.base_url        = opts["target_url"]
+		self.user_agent      = opts["user_agent"]
+		self.extracted_links = set()
+		self.queue           = Queue()
+		self.scope           = get_domain_name(self.base_url)
+		self.worker_loop     = True
 
-def clean_link(link):
-	scheme = link.split("//")[0] + "//"
-	link_cleaned = scheme + link.replace(scheme, "").replace("//", "/")
-	return link_cleaned
+		if self.user_agent == "default":
+			self.user_agent = "Mozilla/5.0"
+		
+		self.headers = {"User-Agent":self.user_agent}
 
-def print_links(html_contents, target_url):
-	scoped_urls = []
-	soup = BeautifulSoup(html_contents, "html.parser")
-	links_extracted = soup.find_all('a')
-	for link in links_extracted:
+		printdbg(f"[+] Booting first crawl for {self.base_url}")
+		printdbg(f"[+] Scope is: {self.scope}")
+		self.crawl_page(self.base_url)
+		self.set_2_queue()
+
+	def set_2_queue(self):
+		for link in self.extracted_links:
+			if is_valid_url(link):
+				self.queue.put(link)
+
+	def crawl_page(self, page_url):
 		try:
-			link_href = link["href"]
-			if not is_valid_url(link_href):
-				link_href = clean_link(f"{target_url}{link_href}")
-			if link_href.startswith(target_url):
-				scoped_urls.append(link_href)
-			print(link_href)
-		except KeyError:
-			continue
+			if is_in_scope(page_url, self.scope):
+				printdbg(f"[+] Requesting url: {page_url}")
+				response = requests.get(page_url, headers=self.headers)
+				if "text/html" in response.headers["content-type"]:
+					self.extract_links(response.text, page_url)
+			else:
+				printdbg(f"[+] {page_url} not in scope")
+		except Exception as e:
+			printdbg(f"[+] Can not crawl {page_url}")
+			printdbg(f"[EXCEPTION]: {e}")
+	
+	def extract_links(self, html_contents, base_url):
+		self.extracted_links.clear()
+		soup = BeautifulSoup(html_contents, "html.parser")
+		a_tags = soup.find_all("a")
+		for attr in a_tags:
+			link = attr.get("href")
+			if not link:
+				printdbg(f"[+] Invalid url: {str(link)}")
+				continue
+			url = urljoin(self.base_url, link)
+			if is_valid_url(url) and self.worker_loop:
+				print(url)
+			if is_in_scope(url, self.scope):
+				self.extracted_links.add(url)
 
-	return scoped_urls
+	def worker(self):
+		while self.worker_loop:
+			if self.queue.empty():
+				printdbg("[+] Empty Queue")
+				self.worker_loop = False
+			else:
+				page_url = self.queue.get()
+				self.crawl_page(page_url)
+				self.set_2_queue()
+
+	def run(self):
+		try:
+			thread_list = []
+			for _ in range(self.threads):
+				crawler_thread = threading.Thread(target=self.worker)
+				thread_list.append(crawler_thread)
+				crawler_thread.daemon = True
+				crawler_thread.start()
+			
+			for thr in thread_list:
+				thr.join()
+
+		except KeyboardInterrupt:
+			self.worker_loop = False
+			printdbg(f"\r[+] Keyboard Interrupt detected. Exiting")
+			sys.exit()
+
+
+def printdbg(message):
+	if debug_messages:
+		print(colored(message, "red"))
+
+def get_domain_name(url):
+	results = urlparse(url).netloc
+	results =  results.split(".")
+	return results[-2] + "." + results[-1]
+
+def is_in_scope(page_url, scope_domain):
+	pattern = re.compile(r"https?://(?:\w+\.)?" + re.escape(scope_domain) + r"/?.*")
+	return bool(pattern.match(page_url))
+
+def is_valid_url(url):
+	scheme = re.compile("^https?://[a-z 0-9]")
+	return bool(scheme.match(url))
 
 if __name__ == "__main__":
-	if not(len(sys.argv) == 2):
-		print("[!] Make sure to provide a target url.")
-		print(f"[!] Example: {sys.argv[0]} https://target.com")
-	else:
-		target_url = sys.argv[1]
-		print(f"[+] Starting crawl on {target_url}")
-		html_contents = do_get(target_url)
-		if html_contents:
-			print(f"[+] Printing gathered links...\n------")
-			scoped_urls = print_links(html_contents, target_url)
-			print("-------\n[+] Scoped urls following:")
-			for scoped in scoped_urls:
-				print(scoped)
+	try:
+		parser = argparse.ArgumentParser()
+		parser.add_argument("target_url", type=str, help="Target url to crawl")
+		parser.add_argument("-t", dest="threads", type=int, default=4, help="Number of threads to run (default=4)")
+		parser.add_argument("-a", dest="user_agent", default="default", type=str, help="Set a custom User-Agent string")
+		parser.add_argument("-d", dest="debug_messages", action="store_true", help="Show debug messages (default=false)")
+		args = parser.parse_args()
+		opts = vars(args)
+		debug_messages = opts["debug_messages"]
+		crawley = Crawley(opts)
+		printdbg("[+] Starting Crawley.run()")
+		crawley.run()
+	except KeyboardInterrupt:
+		printdbg(f"\r[+] Keyboard Interrupt detected. Exiting")
+		sys.exit()
+		
